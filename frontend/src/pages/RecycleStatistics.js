bcd36f15ba2bae28de00bff6a23c335d28f58f14
Felipe Vieira <102335240+Felipe-Vieira-03@users.noreply.github.com>
2023-11-03 18:14:16 -0300

    feat: adding call from new page

--- frontend/src/pages/RecycleStatistics.js
+++ frontend/src/pages/RecycleStatistics.js
@@ -18,26 +18,25 @@ class RecycleStatistics extends React.Component {
             .catch(err=> { console.error("Error fetching data:", err); });
     };
 
-    render(){
-        return (
-            <div className="states">
-                <h1 className="statesTitle"> Reciclagem por Estado 2020/2021 </h1>
-                <div className="stateList">
-                    {this.state.details.map((stateOccurrence, key) => {
-                        return (
-                            <StateOccurrence
-                                key={key}
-                                state={stateOccurrence.state}
-                                paper={stateOccurrence.paper}
-                                steel={stateOccurrence.steel}
-                                glass={stateOccurrence.glass}
-                                cooperatives={stateOccurrence.cooperatives}
-                            />
-                        );
-                    })}
-                </div>
-            </div>
-        );
+    render() {
+      return (
+        <div className="states">
+          <h1 className="statesTitle">Reciclagem por Estado 2020/2021</h1>
+          <div className="stateList">
+            {this.state.details.map((stateOccurrence, key) => (
+              <Link to={`/state/${stateOccurrence.state}`} key={key} style={{ textDecoration: 'none', cursor: 'pointer', color: 'black' }}>
+                <StateOccurrence
+                  state={stateOccurrence.state}
+                  paper={stateOccurrence.paper}
+                  steel={stateOccurrence.steel}
+                  glass={stateOccurrence.glass}
+                  cooperatives={stateOccurrence.cooperatives}
+                />
+              </Link>
+            ))}
+          </div>
+        </div>
+      );
     };
 }
 
