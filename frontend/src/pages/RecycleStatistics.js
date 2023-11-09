67d6d95d63e057cf11e268265e62212bc81ac07b
leandro.alves14 <=>
2023-11-09 15:10:45 -0300

    feat: add image background to StatesOccurrences

--- frontend/src/pages/RecycleStatistics.js
+++ frontend/src/pages/RecycleStatistics.js
@@ -1,5 +1,6 @@
 import StateOccurrence from '../components/StateOccurrence'
 import "../styles/RecycleStatistics.css";
+import {Link} from 'react-router-dom';
 import React from 'react';
 import axios from 'axios';
 
@@ -26,6 +27,7 @@ class RecycleStatistics extends React.Component {
             {this.state.details.map((stateOccurrence, key) => (
               <Link to={`/state/${stateOccurrence.state}`} key={key} style={{ textDecoration: 'none', cursor: 'pointer', color: 'black' }}>
                 <StateOccurrence
+                  image={stateOccurrence.state_img}
                   state={stateOccurrence.state}
                   paper={stateOccurrence.paper}
                   steel={stateOccurrence.steel}
