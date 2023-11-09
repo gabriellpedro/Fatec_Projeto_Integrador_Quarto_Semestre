db3c68787f08da11440fae276f0b047eabf0a14c
Attenuare <leandro.augusto.alves@outlook.com.br>
2023-11-08 21:58:21 -0300

    feat: new page with styles to see by cities

--- frontend/src/pages/CitiesStatistics.js
+++ frontend/src/pages/CitiesStatistics.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import axios from 'axios';
-import '../styles/RecycleStatistics.css';
+import '../styles/CitysOccurrences.css';
+import withRouter from '../components/Hoc';
+import CityOccurrence from '../components/CityOccurrence'
 
 class CitiesStatistics extends React.Component {
-  state = {
-    cityData: null
-  };
+  state = { details: [], };
+
+  // ParamsUsed = () => {
+  //     const params = useParams();
+  //     return params
+  // }
 
   componentDidMount() {
+    // let params = this.ParamsUsed();
     let data;
     axios
-      .get('http://localhost:8000/materials/getstatistics')
+      .get(`http://localhost:8000/materials/reciclestatistics/${this.props.params.state}`)
       .then((res) => {
         data = res.data;
         this.setState({
@@ -22,30 +28,58 @@ class CitiesStatistics extends React.Component {
       });
   }
 
-  renderCityData() {
-    const { cityData } = this.state;
-
-    if (cityData) {
-      return (
-        <div>
-          <h1>City Statistics</h1>
-          <p>City Name: {cityData.cityName}</p>
-          <p>Population: {cityData.population}</p>
-          {/* need to render the properties here */}
-        </div>
-      );
-    }
-
-    return <p>Carregando informações das cidades...</p>;
-  }
-
   render() {
-    return (
-      <div>
-        {this.renderCityData()}
-      </div>
+      return (
+        <div className="citys">
+            <h1 className="citysTitle"> Recicláveis da cidade de {this.props.params.state} </h1>
+            <div className="citysList">
+                {this.state.details.map((CitysOccurrence, key) => {
+                    return (
+                        <CityOccurrence
+                          key={key}
+                          city={CitysOccurrence.city}
+                          state={CitysOccurrence.state}
+                          plastic={CitysOccurrence.plastic}
+                          paper={CitysOccurrence.paper}
+                          steel={CitysOccurrence.steel}
+                          glass={CitysOccurrence.glass}
+                          cooperatives={CitysOccurrence.cooperatives}
+                        />
+                    );
+                })}
+            </div>
+        </div>
     );
-  }
-}
+  };
+};
 
-export default CitiesStatistics;
+
+
+
+//   renderCityData() {
+//     const { cityData } = this.state;
+
+//     if (cityData) {
+//       return (
+//         <div>
+//           <h1>City Statistics</h1>
+//           <p>City Name: {cityData.cityName}</p>
+//           <p>Population: {cityData.population}</p>
+//           {/* need to render the properties here */}
+//         </div>
+//       );
+//     }
+
+//     return <p>Carregando informações das cidades...</p>;
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.renderCityData()}
+//       </div>
+//     );
+//   }
+// }
+
+export default withRouter(CitiesStatistics);
