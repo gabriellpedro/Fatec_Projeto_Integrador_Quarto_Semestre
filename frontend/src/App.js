6411aca134fa962206d3a776fbe2995435bf271e
leandro.alves14 <=>
2023-11-15 11:48:55 -0300

    feat: add login and register components with berry template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import React from 'react';
 import CitiesStatistics from './pages/CitiesStatistics';
+import Login from './pages/authentication/authentication/Login'
+import Register from './pages/authentication/authentication/Register'
 
 class App extends React.Component {
   render(){
@@ -19,6 +21,8 @@ class App extends React.Component {
             <Route path="/getmaterials" exact element={<RecycleMaterials />} />
             <Route path="/reciclestatistics" exact element={<RecycleStatistics />} />
             <Route path="/reciclestatistics/:state" exact element={<CitiesStatistics/>} />
+            <Route path="/user/login" exact element={<Login/>} />
+            <Route path="/user/register" exact element={<Register/>} />
           </Routes>
           <Footer />
         </Router>
