4a3960fe0574f7d4b7f86b2fc4430d7d710294db
leandro.alves14 <=>
2023-11-23 21:28:59 -0300

    feat: change the handleregister to connect to the backend

--- frontend/src/pages/authentication/auth-forms/AuthRegister.js
+++ frontend/src/pages/authentication/auth-forms/AuthRegister.js
@@ -32,7 +32,7 @@ import useScriptRef from '../../../hooks/useScriptRef';
 import Google from '../../../assets/images/icons/social-google.svg';
 import AnimateButton from '../../../components/extended/AnimateButton';
 import { strengthColor, strengthIndicator } from '../../../utils/password-strength';
-
+import { useNavigate } from 'react-router-dom';
 // assets
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
@@ -46,6 +46,7 @@ const FirebaseRegister = ({ ...others }) => {
   const customization = useSelector((state) => state.customization);
   const [showPassword, setShowPassword] = useState(false);
   const [checked, setChecked] = useState(true);
+  const navigate = useNavigate();
 
   const [strength, setStrength] = useState(0);
   const [level, setLevel] = useState();
@@ -71,6 +72,45 @@ const FirebaseRegister = ({ ...others }) => {
   useEffect(() => {
     changePassword('123456');
   }, []);
+
+  const handleRegister = async (values, { setErrors, setStatus, setSubmitting }) => {
+    
+    // const handleSuccess = () => {
+    //   successNotification('Registro Realizado com Sucesso!');
+    // };
+
+    try {
+      const response = await fetch('http://localhost:8000/materials/register/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      });
+      console.log(JSON.stringify(values))
+      console.log('RESPONSE STATUS', response.ok)
+      console.log('SCRIPT STATUS', scriptedRef.current)
+      // if (scriptedRef.current) {
+        const data = await response.json();
+        if (response.ok) {
+          setStatus({ success: true });
+          localStorage.setItem('notification', JSON.stringify({ message: 'Registro Realizado com Sucesso!', type: 'success' }));
+          navigate('/user/login/');
+        } else {
+          setStatus({ success: false });
+          setErrors({ submit: data.message || 'Register failed' });
+        }
+        setSubmitting(false);
+      // }
+    } catch (err) {
+      console.error(err);
+      if (scriptedRef.current) {
+        setStatus({ success: false });
+        setErrors({ submit: err.message });
+        setSubmitting(false);
+      }
+    }
+  };
 
   return (
     <>
@@ -127,56 +167,55 @@ const FirebaseRegister = ({ ...others }) => {
 
       <Formik
         initialValues={{
+          name: '',
           email: '',
           password: '',
+          confirm_password: '',
           submit: null
         }}
         validationSchema={Yup.object().shape({
-          email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
-          password: Yup.string().max(255).required('Password is required')
+          name: Yup.string().max(255).required('Necessário Usuário'),
+          email: Yup.string().email('Must be a valid email').max(255).required('Necessário Email'),
+          password: Yup.string().max(255).required('Necessário uma senha'),
+          confirm_password: Yup.string()
+          .oneOf([Yup.ref('password'), null], 'Senhas não coincidem') // Custom validation function
+          .required('Senhas não coincidem')
         })}
-        onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
-          try {
-            if (scriptedRef.current) {
-              setStatus({ success: true });
-              setSubmitting(false);
-            }
-          } catch (err) {
-            console.error(err);
-            if (scriptedRef.current) {
-              setStatus({ success: false });
-              setErrors({ submit: err.message });
-              setSubmitting(false);
-            }
-          }
-        }}
+        onSubmit={handleRegister}
       >
         {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
           <form noValidate onSubmit={handleSubmit} {...others}>
-            <Grid container spacing={matchDownSM ? 0 : 2}>
-              <Grid item xs={12} sm={6}>
+            {/* <Grid container spacing={matchDownSM ? 0 : 2}>
+              <Grid item xs={12} sm={12}>
                 <TextField
                   fullWidth
-                  label="First Name"
+                  label="Usuário"
                   margin="normal"
                   name="fname"
                   type="text"
+                  value={values.name}
                   defaultValue=""
                   sx={{ ...theme.typography.customInput }}
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="Last Name"
-                  margin="normal"
-                  name="lname"
-                  type="text"
-                  defaultValue=""
-                  sx={{ ...theme.typography.customInput }}
-                />
-              </Grid>
-            </Grid>
+            </Grid> */}
+            <FormControl fullWidth error={Boolean(touched.name && errors.name)} sx={{ ...theme.typography.customInput }}>
+              <InputLabel htmlFor="outlined-adornment-username-register">Usuário</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-username-register"
+                type="username"
+                value={values.name}
+                name="name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                inputProps={{}}
+              />
+              {touched.name && errors.name && (
+                <FormHelperText error id="standard-weight-helper-text--register">
+                  {errors.name}
+                </FormHelperText>
+              )}
+            </FormControl>
             <FormControl fullWidth error={Boolean(touched.email && errors.email)} sx={{ ...theme.typography.customInput }}>
               <InputLabel htmlFor="outlined-adornment-email-register">Email</InputLabel>
               <OutlinedInput
@@ -223,7 +262,7 @@ const FirebaseRegister = ({ ...others }) => {
                 }
                 inputProps={{}}
               />
-              {touched.password && errors.password && (
+              {Boolean(touched.password && errors.password) && (
                 <FormHelperText error id="standard-weight-helper-text-password-register">
                   {errors.password}
                 </FormHelperText>
@@ -246,6 +285,59 @@ const FirebaseRegister = ({ ...others }) => {
                 </Box>
               </FormControl>
             )}
+
+<FormControl fullWidth error={Boolean(touched.confirm_password && errors.confirm_password)} sx={{ ...theme.typography.customInput }}>
+              <InputLabel htmlFor="outlined-adornment-confirm-password-register">Confirmar a Senha</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-confirm-password-register"
+                type={showPassword ? 'text' : 'password'}
+                value={values.confirm_password}
+                name="confirm_password"
+                label="confirm_password"
+                onBlur={handleBlur}
+                onChange={(e) => {
+                  handleChange(e);
+                  // changePassword(e.target.value);
+                }}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle confirm password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                      size="large"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                inputProps={{}}
+              />
+              {(values.password !== values.confirm_password) && (
+                <FormHelperText error id="standard-weight-helper-text-confirm-password-register">
+                  {errors.confirm_password}
+                </FormHelperText>
+              )}
+            </FormControl>
+{/*
+            {strength !== 0 && (
+              <FormControl fullWidth>
+                <Box sx={{ mb: 2 }}>
+                  <Grid container spacing={2} alignItems="center">
+                    <Grid item>
+                      <Box style={{ backgroundColor: level?.color }} sx={{ width: 85, height: 8, borderRadius: '7px' }} />
+                    </Grid>
+                    <Grid item>
+                      <Typography variant="subtitle1" fontSize="0.75rem">
+                        {level?.label}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </FormControl>
+            )} */}
+
 
             <Grid container alignItems="center" justifyContent="space-between">
               <Grid item>
