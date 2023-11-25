3c2307a6c569df72e9635f8ae869c1f8b1836098
Felipe Vieira <102335240+Felipe-Vieira-03@users.noreply.github.com>
2023-11-25 18:46:54 -0300

    feat: add new page 'AboutUs'

---
+++ frontend/src/pages/AboutUs.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import '../styles/AboutUs.css';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h2>Sobre Nós</h2>
+      <p>
+        Bem-vindo à nossa iniciativa dedicada à implementação de ecopontos sustentáveis!
+      </p>
+      <p>
+        Somos um projeto de caráter sustentável que visa não apenas contribuir para a preservação do meio ambiente, mas também buscar conhecimento por meio desta empreitada. 
+        Comprometemo-nos de maneira integral com a preservação do meio ambiente, buscando a redução do impacto negativo causado pelos resíduos. 
+        Nossos ecopontos foram meticulosamente projetados para facilitar a separação e o descarte adequado de diferentes tipos de materiais.
+
+        Convidamos você a se unir a nós nessa jornada em prol de um planeta mais limpo e sustentável. Ao colaborar conosco, você estará fazendo a diferença!
+      </p>
+      <p>
+        O cerne do nosso projeto reside na dualidade de objetivos: contribuir para a preservação ambiental e proporcionar um retorno lucrativo a todos os que decidirem 
+        colaborar conosco.
+        Almejamos a sustentabilidade em todos os aspectos, incluindo a participação ativa da sociedade na conservação do meio ambiente.
+      </p>
+      <p>
+        Somos um grupo de estudantes da Faculdade de Tecnologia Antonio Brambilla, FATEC de Araras, e estamos atualmente matriculados no curso de
+        Desenvolvimento de Software Multiplataforma. Recebemos a instigante proposta do Instituto PIPA para integrar o descarte adequado de materiais 
+        a um retorno tangível para os colaboradores, tornando nosso projeto não apenas promissor, mas também uma valiosa oportunidade de aprendizado e avaliação diferenciada.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
