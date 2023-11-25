ca00a6c214f07736f9dc26bd69017622b66a5dc6
leandro.alves14 <=>
2023-11-25 18:54:33 -0300

    feat: add user scale to frontend

---
+++ frontend/src/hooks/BackRequestUserBalance.js
@@ -0,0 +1,37 @@
+import BalanceOccurrenceSmall from "../components/BalanceOccurrenceSmall"
+import { React } from 'react';
+
+
+async function HandleUserBalance(setoperationsBalance) {
+  const storedUserOperationsBalance = localStorage.getItem('selecteduserbalance');
+  console.log("In the handle balance component")
+  var foundUserOperationsBalance = [];
+
+    try{
+      if (storedUserOperationsBalance) {
+          foundUserOperationsBalance = JSON.parse(storedUserOperationsBalance);
+
+          console.log('STORAGED BALANCE', foundUserOperationsBalance )
+          if (foundUserOperationsBalance.length > 0){
+            foundUserOperationsBalance = foundUserOperationsBalance[0];
+            if (foundUserOperationsBalance.recicle_balance_array.length > 0){
+              console.log('SIMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+              console.log(foundUserOperationsBalance.recicle_balance_array[0].mesure)
+              const operationsBalance = foundUserOperationsBalance.recicle_balance_array.map((balanceOccurrence, key) => (
+                <BalanceOccurrenceSmall
+                  key={key}
+                  name="Tests Worked"
+                  mesure={balanceOccurrence.mesure}
+                  price={balanceOccurrence.price}
+                  mesure_unity={balanceOccurrence.mesure_unity}/>
+              ));
+              setoperationsBalance(operationsBalance);
+            }
+          }
+        };
+    } catch(error){
+      console.error('Error parsing storedUserOperations:', error);
+    }
+  };
+  
+export default HandleUserBalance;
