3a0d327e8856b90bcca6c095e0a5207f4b0964f7
gabriellpedro <gabriel.pedro47@hotmail.com>
2024-03-17 14:43:01 -0300

    feat: adding the login page

---
+++ mobile/ecoponto/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:ecoponto/pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EcoPonto',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
