9334ee74379d1b57f4702581d3a764f3e46eae5c
leandro_alves14 <leandro.alves@nappsolutions.com>
2024-06-06 09:07:16 -0300

    feat: adjust project struture and add itembag

---
+++ ecoponto/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import location
+import video_player_avfoundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+}
